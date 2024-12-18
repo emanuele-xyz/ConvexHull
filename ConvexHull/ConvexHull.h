88778534fbe48b140653d4942ff50c8f054e17b7
emanuele franchi <me.emanuele.xyz@gmail.com>
2024-12-18 15:19:02 +0100

    first commit

---
+++ ConvexHull/ConvexHull.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace ch
+{
+    // TODO: declearations go here
+
+    #if defined(CONVEX_HULL_IMPLEMENTATION)
+    // TODO: definitions go here
+    #endif
+}
---
+++ Playground/Main.cpp
@@ -0,0 +1,11 @@
+#define CONVEX_HULL_IMPLEMENTATION
+#include <ConvexHull.h>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from Convex Hull!" << std::endl;
+
+    return 0;
+}
