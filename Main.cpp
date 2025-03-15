2e4dc2a0bffec2cfa34b669ac5e061132382880a
0xHaru <0xharu.git@gmail.com>
2025-03-15 12:22:58 +0100

    Added collinearity test during points generation

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <random>
@@ -5,28 +6,65 @@
 
 #include <ConvexHull.h>
 
+// returns the determinant of the matrix [u | v]
+static double determinant(ch::v2 u, ch::v2 v)
+{
+    return u.x * v.y - u.y * v.x;
+}
+
+static bool are_collinear(ch::v2 u, ch::v2 v, ch::v2 w, double threshold = 1.0)
+{
+    ch::v2 u_to_v{ v - u };
+    ch::v2 u_to_w{ w - u };
+    return std::abs(determinant(u_to_v, u_to_w)) < threshold;
+}
+
+static std::vector<ch::v2> generate_points(int points_count)
+{
+    std::random_device random_device{};
+    std::mt19937 generator{ random_device() };
+    std::uniform_real_distribution<> distribution{ 0.0, 100.0 };
+
+    std::vector<ch::v2> points(points_count);
+
+    for (int i{}; i < points_count; i++)
+    {
+        double x{ distribution(generator) };
+        double y{ distribution(generator) };
+        ch::v2 p{ x, y };
+
+        bool any_collinear{ false };
+        for (int j{}; j < i && !any_collinear; j++)
+        {
+            for (int k{}; k < i && !any_collinear; k++)
+            {
+                if (j == k) continue;
+
+                if (are_collinear(p, points[j], points[k], 3))
+                {
+                    i--;
+                    any_collinear = true;
+                }
+            }
+        }
+
+        points[i] = p;
+    }
+
+    return points;
+}
+
 int main()
 {
     std::string out_path{ "test.txt" };
     std::ofstream out_file{ out_path };
     if (out_file)
     {
-        std::random_device random_device{};
-        std::mt19937 generator{ random_device() };
-        std::uniform_real_distribution<> distribution{ 0.0, 100.0 };
-
+        // generate points
         int points_count{ 100 };
-        std::vector<ch::v2> points(points_count);
+        std::vector<ch::v2> points{ generate_points(points_count) };
         std::vector<ch::v2> hull(points_count);
         std::vector<int> adj_matrix(points_count * points_count);
-
-        // generate points
-        for (size_t i{}; i < points.size(); i++)
-        {
-            double x{ distribution(generator) };
-            double y{ distribution(generator) };
-            points[i] = ch::v2{ x, y };
-        }
 
         int hull_count{ ch::naive(points_count, points.data(), hull.data(), adj_matrix.data()) };
 
