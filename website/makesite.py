c499ac74405c160617fcd6e16da4b8a08c0de8c7
0xHaru <0xharu.git@gmail.com>
2025-04-02 17:04:17 +0200

    Changed website/ directory structure and worked on the homepage

--- website/makesite.py
+++ website/makesite.py
@@ -19,8 +19,8 @@ def pandoc(f):
     subprocess.run(
         shlex.split(
             f"pandoc --standalone --mathjax --from markdown --to html5 "
-            f"--include-in-header common/header.html "
-            f"--include-before-body common/body.html "
+            f"--include-in-header markdown/common/header.html "
+            f"--include-before-body markdown/common/body.html "
             f"--css style.css "
             f"--output docs/{relative_filepath}.html {f}"
         )
@@ -28,29 +28,22 @@ def pandoc(f):
 
 
 def main():
-    # Create a new docs directory from scratch
+    # Create a new "docs" directory from scratch
     if os.path.isdir("docs"):
         shutil.rmtree(f"docs")
     shutil.copytree(f"static", f"docs")
-
-    # Mimic the directory structure of content/ in docs/
-    content_subdirs = glob.glob(f"content/**/", recursive=True)
-    content_subdirs.remove("content/")
-
-    for d in content_subdirs:
-        d = d.replace("content", "docs", 1)
-        if not os.path.isdir(d):
-            os.makedirs(d)
 
     # Create .nojekyll in docs/
     with open("docs/.nojekyll", "w"):
         pass
 
-    html_files = glob.glob(f"content/**/*.html", recursive=True)
+    # Copy HTML files from html/ to docs/.
+    html_files = glob.glob(f"html/*.html", recursive=True)
     for f in html_files:
         shutil.copy(f, f"docs/{os.path.basename(f)}")
 
-    markdown_files = glob.glob(f"content/**/*.md", recursive=True)
+    # Convert MD files to HTML and copy them to docs/
+    markdown_files = glob.glob(f"markdown/*.md", recursive=True)
     for f in markdown_files:
         pandoc(f)
 
