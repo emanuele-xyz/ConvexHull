ccb97f6bd1216a35058fa18d9000c488b5ca8b6c
emanuele franchi <me.emanuele.xyz@gmail.com>
2025-04-07 18:27:00 +0200

    test akl toussaint

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,10 @@
 #include <cassert>
+#include <chrono>
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <format>
+#include <functional>
 #include <random>
 #include <vector>
 
@@ -117,7 +120,149 @@ static void dump_points_and_hull(const std::vector<ch::v2>& points, const std::v
     }
 }
 
-#if 1
+template<typename Duration>
+std::string format_duration(Duration d)
+{
+    using namespace std::chrono;
+
+    // Convert the duration to various units
+    auto ns = duration_cast<nanoseconds>(d).count();
+    auto us = duration_cast<microseconds>(d).count();
+    auto ms = duration_cast<milliseconds>(d).count();
+    auto s = duration_cast<seconds>(d).count();
+
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(2);
+
+    if (ns < 1000)
+    {
+        oss << ns << " ns";
+    }
+    else if (us < 1000)
+    {
+        // convert to microseconds
+        oss << us << " us";
+    }
+    else if (ms < 1000)
+    {
+        // convert to milliseconds
+        oss << ms << " ms";
+    }
+    else
+    {
+        oss << s << " s";
+    }
+
+    return oss.str();
+}
+
+
+class Logger
+{
+public:
+    Logger();
+    ~Logger() = default;
+    Logger(const Logger&) = delete;
+    Logger(Logger&&) noexcept = delete;
+    Logger& operator=(const Logger&) = delete;
+    Logger& operator=(Logger&&) noexcept = delete;
+public:
+    void log(const std::string& msg);
+    template<typename... Args>
+    void logf(const std::format_string<Args...> fmt, Args&&... args)
+    {
+        log(std::vformat(fmt.get(), std::make_format_args(args...)));
+    }
+private:
+    std::ofstream m_log_file;
+};
+
+Logger::Logger()
+    : m_log_file{ "log.txt" }
+{
+}
+void Logger::log(const std::string& msg)
+{
+    std::cout << msg << std::flush;
+    m_log_file << msg;
+    m_log_file.flush();
+}
+
+static std::vector<ch::v2> generate_dataset(Logger& logger, int capacity)
+{
+    std::vector<ch::v2> dataset{};
+    {
+        logger.log("generating points ... ");
+        auto start{ std::chrono::high_resolution_clock::now() };
+        dataset = generate_points(capacity);
+        auto end{ std::chrono::high_resolution_clock::now() };
+        auto duration_str{ format_duration(end - start) };
+        logger.logf("DONE ... {}\n", duration_str);
+    }
+    return dataset;
+}
+
+using HullFn = std::function<std::vector<ch::v2>(std::vector<ch::v2>)>;
+static void test(Logger& logger, std::vector<ch::v2> dataset, HullFn oracle, const std::string& oracle_name, HullFn func, const std::string& func_name)
+{
+    std::random_device rd{};
+    std::mt19937 gen{ rd() };
+    std::uniform_int_distribution<> distrib{ 5, 100 };
+    int points_count{ distrib(gen) };
+    while (points_count < static_cast<int>(dataset.size()))
+    {
+        logger.log("--------------------------------------------------------------------------------\n");
+        logger.logf("points count: {}\n", points_count);
+
+        // copy portion of point dataset
+        std::vector<ch::v2> points{ dataset.begin(), dataset.begin() + points_count };
+
+        logger.logf("{} ... ", oracle_name);
+        std::vector<ch::v2> oracle_hull{};
+        {
+            auto start{ std::chrono::high_resolution_clock::now() };
+            oracle_hull = oracle(points);
+            auto end{ std::chrono::high_resolution_clock::now() };
+            auto us{ std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() };
+            logger.logf("DONE ... {} us\n", us);
+        }
+
+        logger.logf("{} ... ", func_name);
+        std::vector<ch::v2> func_hull{};
+        {
+            auto start{ std::chrono::high_resolution_clock::now() };
+            func_hull = func(points);
+            auto end{ std::chrono::high_resolution_clock::now() };
+            auto us{ std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() };
+            logger.logf("DONE ... {} us\n", us);
+        }
+
+        if (validate_hull(oracle_hull, func_hull))
+        {
+            logger.log("hulls MATCH\n");
+        }
+        else
+        {
+            logger.log("hulls DON'T MATCH\n");
+        }
+
+        points_count += distrib(gen);
+    }
+}
+
+static void test_dc_akl_toussaint()
+{
+    Logger logger{};
+
+    int dataset_capacity{ 10000 };
+    logger.log("--------------------------------------------------------------------------------\n");
+    logger.logf("dataset capacity: {}\n", dataset_capacity);
+
+    std::vector<ch::v2> dataset{ generate_dataset(logger, dataset_capacity) };
+    test(logger, dataset, ch::divide_and_conquer, "divide_and_conquer", ch::akl_toussaint, "akl_toussaint");
+}
+
+#if 0
 int main()
 {
     // generate points
@@ -144,56 +289,6 @@ int main()
 #else
 int main()
 {
-    std::random_device rd{};
-    std::mt19937 gen{ rd() };
-    std::uniform_int_distribution<> distrib{ 5, 100 };
-    std::ofstream log{ "log.txt" };
-    int points_count{ distrib(gen) };
-    while (true)
-    {
-        std::cout << "--------------------------------------------------------------------------------\n";
-        log << "--------------------------------------------------------------------------------\n";
-        log.flush();
-        std::cout << "points count: " << points_count << "\n";
-        log << "points count: " << points_count << "\n";
-        log.flush();
-        std::cout << "generating points ... ";
-        log << "generating points ... ";
-        log.flush();
-        std::vector<ch::v2> points{ generate_points(points_count) };
-        std::cout << "DONE\n";
-        log << "DONE\n";
-        log.flush();
-        std::cout << "naive hull ... ";
-        log << "naive hull ... ";
-        log.flush();
-        std::vector<ch::v2> naive_hull{ ch::naive(points) };
-        std::cout << "DONE\n";
-        log << "DONE\n";
-        log.flush();
-        std::cout << "divide and conquer ... ";
-        log << "divide and conquer ... ";
-        log.flush();
-        std::vector<ch::v2> found_hull{ ch::divide_and_conquer(points) };
-        std::cout << "DONE\n";
-        log << "DONE\n";
-        log.flush();
-        if (validate_hull(naive_hull, found_hull))
-        {
-            std::cout << "hulls MATCH\n";
-            log << "hulls MATCH\n";
-            log.flush();
-        }
-        else
-        {
-            std::cout << "hulls DON'T MATCH\n";
-            log << "hulls DON'T MATCH\n";
-            log.flush();
-        }
-
-        points_count += distrib(gen);
-    }
-
-    return 0;
+    test_dc_akl_toussaint();
 }
 #endif
