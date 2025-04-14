0b1fc61a66123e68d031e9edad34cf97d161d142
0xHaru <0xharu.git@gmail.com>
2025-04-14 13:47:05 +0200

    [WIP] Started working on Divide and Conquer visualization

--- website/static/convex_hull.js
+++ website/static/convex_hull.js
@@ -114,6 +114,258 @@ class Naive {
   }
 }
 
+class DCStackFrame {
+  constructor(points) {
+    // 5 states: divide, convex-hull-left, convex-hull-right, merge, done
+    this.state = "divide";
+
+    this.points = [...points];
+    this.points.sort((a, b) => a.x <= b.x);
+
+    this.half = Math.trunc(this.points.length / 2);
+
+    this.leftHull = [];
+    this.rightHull = [];
+    this.hull = [];
+  }
+}
+
+class DivideAndConquer {
+  constructor() {
+    this.stackIndex = 0;
+    this.stack = [];
+    this.stack.push(new DCStackFrame(points));
+  }
+
+  getNextIndexCw(i, size) {
+    return (i + 1) % size;
+  }
+
+  getNextIndexCcw(i, size) {
+    return (((i - 1) % size) + size) % size;
+  }
+
+  getIntersectY(middleX, p, q) {
+    const m = (p.y - q.y) / (p.x - q.x);
+    const b = p.y - m * p.x; // b = y - mx
+    return m * middleX + b; // y = mx + b
+  }
+
+  merge() {
+    const frame = this.stack[this.stackIndex];
+
+    console.assert(frame.leftHull.length > 0);
+    console.assert(frame.rightHull.length > 0);
+
+    // find rightmost point of a and leftmost point of b
+    const leftmostIndex = 0;
+    const rightmostIndex = 0;
+    {
+      for (let i = 1; i < frame.rightHull.length; i++) {
+        if (frame.rightHull[i].x < frame.rightHull[leftmostIndex].x) {
+          leftmostIndex = i;
+        }
+      }
+
+      for (let i = 1; i < frame.leftHull.length; i++) {
+        if (frame.rightHull[i].x > frame.rightHull[rightmostIndex].x) {
+          rightmostIndex = i;
+        }
+      }
+    }
+
+    const middleLine =
+      (frame.leftHull[rightmostIndex].x + frame.rightHull[leftmostIndex].x) / 2; // middle vertical line (the x value) separating the two hulls
+
+    // find upper tangent
+    const upperTanLeftIndex = 0;
+    const upperTanRightIndex = 0;
+    {
+      let i = rightmostIndex;
+      let j = leftmostIndex;
+
+      while (true) {
+        let nextI = this.getNextIndexCcw(i, frame.leftHull.length);
+        let nextJ = this.getNextIndexCw(j, frame.rightHull.length);
+
+        if (
+          this.getIntersectY(
+            middleLine,
+            frame.leftHull[nextI],
+            frame.rightHull[j]
+          ) >
+          this.getIntersectY(middleLine, frame.leftHull[i], frame.rightHull[j])
+        ) {
+          i = nextI;
+        } else if (
+          this.getIntersectY(
+            middle_line,
+            frame.leftHull[i],
+            frame.rightHull[next_j]
+          ) >
+          this.getIntersectY(middle_line, frame.leftHull[i], frame.rightHull[j])
+        ) {
+          j = nextJ;
+        } else {
+          break;
+        }
+      }
+
+      upperTanLeftIndex = i;
+      upperTanRightIndex = j;
+    }
+
+    // find lower tangent
+    const lowerTanLeftIndex = 0;
+    const lowerTanRightIndex = 0;
+    {
+      let i = rightmostIndex;
+      let j = leftmostIndex;
+
+      while (true) {
+        let nextI = this.getNextIndexCw(i, frame.leftHull.length);
+        let nextJ = this.getNextIndexCcw(j, frame.rightHull.length);
+
+        if (
+          this.getIntersectY(
+            middleLine,
+            frame.leftHull[nextI],
+            frame.rightHull[j]
+          ) >
+          this.getIntersectY(middleLine, frame.leftHull[i], frame.rightHull[j])
+        ) {
+          i = nextI;
+        } else if (
+          this.getIntersectY(
+            middle_line,
+            frame.leftHull[i],
+            frame.rightHull[next_j]
+          ) >
+          this.getIntersectY(middle_line, frame.leftHull[i], frame.rightHull[j])
+        ) {
+          j = nextJ;
+        } else {
+          break;
+        }
+      }
+
+      lowerTanLeftIndex = i;
+      lowerTanRightIndex = j;
+    }
+
+    // clockwise merge the two hulls using the found tangents
+    for (
+      let i = lowerTanLeftIndex;
+      i != upperTanLeftIndex;
+      i = this.getNextIndexCw(i, frame.leftHull.length)
+    ) {
+      frame.hull.push(frame.leftHull[i]);
+    }
+
+    frame.hull.push(frame.leftHull[upperTanLeftIndex]);
+
+    for (
+      let j = upperTanRightIndex;
+      j != lowerTanLeftIndex;
+      j = this.getNextIndexCw(j, frame.rightHull.length)
+    ) {
+      frame.hull.push(frame.rightHull[j]);
+    }
+
+    frame.hull.push(frame.rightHull[lowerTanRightIndex]);
+  }
+
+  step() {
+    const frame = this.stack[this.stackIndex];
+    switch (frame.state) {
+      case "divide": {
+        if (frame.points.length <= 1) {
+          frame.state = "done";
+          return;
+        }
+
+        // frame.half = Math.trunc(frame.points.length / 2);
+        frame.state = "convex-hull-left";
+        const slice = frame.points.slice(0, frame.half);
+        this.stack.push(new DCStackFrame(slice));
+        this.stackIndex++;
+        break;
+      }
+      case "convex-hull-left": {
+        const nextFrame = this.stack[this.stackIndex + 1];
+        frame.leftHull = [...nextFrame.hull];
+        this.stack.pop();
+        frame.state = "convex-hull-right";
+        const slice = frame.points.slice(frame.half, frame.points.length);
+        this.stack.push(new DCStackFrame(slice));
+        this.stackIndex++;
+        break;
+      }
+      case "convex-hull-right": {
+        const nextFrame = this.stack[this.stackIndex + 1];
+        frame.rightHull = [...nextFrame.hull];
+        this.stack.pop();
+        frame.state = "merge";
+        this.merge();
+        break;
+      }
+      case "merge": {
+        frame.state = "done";
+        break;
+      }
+      case "done": {
+        this.stackIndex--;
+        break;
+      }
+      default:
+        console.assert(false); // Unreachable
+    }
+  }
+
+  draw() {
+    const frame = this.stack[this.stackIndex];
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (const point of frame.points) {
+      drawPoint(point);
+    }
+
+    switch (frame.state) {
+      case "divide": {
+        if (frame.points.length <= 1) {
+          return;
+        }
+
+        // Divide the canvas using a vertical line.
+        const x =
+          (frame.points[frame.half - 1].x + frame.points[frame.half].x) / 2;
+
+        drawLine({ x: x, y: 0 }, { x: x, y: canvas.height }, "steelblue");
+        break;
+      }
+      case "convex-hull-left": {
+        break;
+      }
+      case "convex-hull-right": {
+        break;
+      }
+      case "merge": {
+        break;
+      }
+      case "done": {
+        break;
+      }
+      default:
+        console.assert(false); // Unreachable
+    }
+  }
+
+  reset() {
+    this.stack = [];
+    this.stack.push(new DCStackFrame(points));
+  }
+}
+
 // algoCtx must have the following methods
 // step() // execute one step of the algorithm
 // draw() // draws the current state of the algorithm
@@ -227,6 +479,10 @@ algoSelect.addEventListener("change", function () {
       algoCtx = new Naive();
       break;
     }
+    case "divide-and-conquer": {
+      algoCtx = new DivideAndConquer();
+      break;
+    }
     default: {
       console.assert(false); // Unreachable
     }
@@ -265,4 +521,9 @@ resetBtn.addEventListener("click", function () {
   points = [];
   algoCtx.reset();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+// Select Naive as the default algorithm.
+window.addEventListener("load", function () {
+  document.getElementById("algoSelect").value = "naive";
 });
